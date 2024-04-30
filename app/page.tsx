c90bf6fdb78f3266500c5f973542497662747ee1
Fabricio <fabricioliquesley15@gmail.com>
2024-04-29 21:32:11 -0300

    feat: add pizza banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -6,9 +7,17 @@ export default function Home() {
   return (
     <>
       <Header />
-      <div className="px-5 pt-6 flex flex-col gap-6">
+      <div className="flex flex-col gap-6 px-5 pt-6">
         <Search />
         <CategoryList />
+        <Image
+          src={"/banner_pizza.png"}
+          alt="Banner promocional, pizzas com até 30% de desconto"
+          height={0}
+          width={0}
+          className="h-auto w-full object-contain"
+          sizes="10ovw"
+        />
       </div>
     </>
   );
