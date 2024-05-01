2fea7525c4c015ee9f416b253115fc2e5b5060fe
Fabricio <fabricioliquesley15@gmail.com>
2024-04-30 21:45:08 -0300

    feat: add product page

---
+++ app/products/[id]/page.tsx
@@ -0,0 +1,46 @@
+import { db } from "@/app/_lib/prisma";
+import { notFound } from "next/navigation";
+import ProductImage from "./_components/product-image";
+import ProductDetails from "./_components/product-details";
+
+interface ProductsPageProps {
+  params: {
+    id: string;
+  };
+}
+
+const ProductsPage = async ({ params: { id } }: ProductsPageProps) => {
+  const product = await db.product.findUnique({
+    where: {
+      id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
+
+  const juices = await db.product.findMany({
+    where: {
+      category: {
+        name: "Sucos",
+      },
+    },
+    take: 10,
+    include: {
+      restaurant: true,
+    },
+  });
+
+  if (!product) {
+    return notFound();
+  }
+
+  return (
+    <div>
+      <ProductImage product={product} />
+      <ProductDetails product={product} complementaryProducts={juices} />
+    </div>
+  );
+};
+
+export default ProductsPage;
